badf33cbd578c6164d6318e783550d2041884590
MVjaters <82DMVjaters@rvt.lv>
2020-02-07 13:48:57 +0200

    a lot of

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-from file_proc 
+from file_proc import read_file, write_file
 
 app = Flask(__name__)
 
@@ -36,37 +36,7 @@ if __name__ == '__main__':
   app.run(host='0.0.0.0', port = 5222, threaded = True, debug = True)
 
 
-# a - Will append to the end of the file
-# w - Will overwrite any existing content
-​
-file_name = 'data.txt'
-​
-  # with open(file_name, 'w') as f:       
-  #     f.write(line)
-def rewrite_file(lines):
-  f = open(file_name, 'w')
-  for line in lines:        
-      f.write(line)
-  f.close()
-​
-def write_file(param):
-  f = open(file_name, 'a')
-  f.write(param + '\n')
-  f.close()
-​
-def read_file():
-  f = open(file_name, 'r')
-  if f.mode == 'r':
-    contents = f.read()
-  else:
-    contents = f"Couldn't read from file {file_name}"
-​
-  return contents
-    
-def read_lines():
-  with open(file_name) as f:
-    lines_array = f.readlines()
-  return lines_array
+
 
 
   
