e157e4095752e03a4a1cfc3e04dae6becca8fbef
MVjaters <82DMVjaters@rvt.lv>
2020-02-14 14:22:34 +0200

    chat lapa

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, json, jsonify
 from file_proc import read_file, write_file
 
 app = Flask(__name__)
@@ -18,6 +18,12 @@ def about():
 @app.route('/contact')
 def contact():
   return render_template('contact.html', phone = 7442457)
+
+@app.route('/chat')
+def chats():
+  return render_template('chats.html')   
+
+
   
 @app.route('/params')
 def params():
@@ -49,7 +55,34 @@ def fileWork():
   elif request.method == 'POST':
     return write_to_file()
   else:
-    return f"Request method {request.method} is not supported!"      
+    return f"Request method {request.method} is not supported!" 
+
+#------------------------SĀKAS CHATS------------------------
+
+@app.route('/chats')
+def index_lapa():
+  return render_template('index.html')
+
+@app.route('/health')
+def health_check():
+  return "Ok"
+
+@app.route('/chats/lasi')
+def ielasit_chatu():
+  chata_rindas = []
+  with open("chats.txt", "r", encoding="UTF-8") as f:
+    for rinda in f:
+      chata_rindas.append(rinda)
+  return jsonify({"chats": chata_rindas})
+
+@app.route('/chats/suuti', methods=['POST'])
+def suutit_zinju():
+  dati = request.json
+
+  with open("chats.txt", "a", newline="", encoding="UTF-8") as f:
+    f.write(dati["chats"] + "\n")
+
+  return ielasit_chatu()                    
 
 
 if __name__ == '__main__':
